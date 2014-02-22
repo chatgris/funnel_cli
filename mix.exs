82b9efef46ba7aabbb97cd02583609ea9bb87a1d
chatgris <julien@stormz.me>
2014-02-22 15:38:59 +0100

    Add Httpotion
    We gonna need to make some http calls.
    
    Signed-off-by: chatgris <jboyer@af83.com>

--- mix.exs
+++ mix.exs
@@ -12,15 +12,12 @@ defmodule FunnelCli.Mixfile do
 
   # Configuration for the OTP application
   def application do
-    []
+    [ applications: [:httpotion]]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
-  # To specify particular versions, regardless of the tag, do:
-  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
   defp deps do
-    []
+    [
+      { :httpotion,   github: "myfreeweb/httpotion" }
+    ]
   end
 end
