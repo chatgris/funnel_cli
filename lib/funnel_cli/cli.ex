7c53c11e34e87179616880b5f9ee3feffa972d0a
chatgris <julien@stormz.me>
2014-05-11 17:05:55 +0200

    [Gardening] Less code on output formatting
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/funnel_cli/cli.ex
+++ lib/funnel_cli/cli.ex
@@ -69,7 +69,7 @@ defmodule FunnelCli.CLI do
       {options, ["index", index_name, body], switches}    -> {:index, index_name, body, options[:name] || switches[:name] || "funnel"}
       {options, ["queries", index_name], switches}        -> {:queries, index_name, options[:name] || switches[:name] || "funnel"}
       {options, ["query", index_name, body], switches}    -> {:query, index_name, body, options[:name] || switches[:name] || "funnel"}
-      {_options, ["listen"], _switches}                    -> {:listen}
+      {_options, ["listen"], _switches}                   -> {:listen}
       {options, ["push", index_name, body], switches}     -> {:push, index_name, body, options[:name] || switches[:name] || "funnel"}
       _                                                   -> :help
     end
@@ -125,45 +125,44 @@ defmodule FunnelCli.CLI do
 
   defp log_out(path, :register) do
     "%{green}File written to #{path}"
-      |> IO.ANSI.escape(true)
-      |> IO.puts
+      |> out
   end
 
   defp log_out(index, :index) do
     "%{green}#{index[:name]} registered as #{index[:id]}"
-      |> IO.ANSI.escape(true)
-      |> IO.puts
+      |> out
   end
 
   defp log_out(message, :error) do
     "%{red}#{message}"
-      |> IO.ANSI.escape(true)
-      |> IO.puts
+      |> out
     System.halt(1)
   end
 
   defp log_out(query, :query, index_name) do
     "%{green}New query added as #{query["query_id"]} to #{index_name} index."
-      |> IO.ANSI.escape(true)
-      |> IO.puts
   end
 
   defp log_out(queries, :queries, index_name) do
     "%{green}#{index_name} queries:"
-      |> IO.ANSI.escape(true)
-      |> IO.puts
+      |> out
     Enum.each(queries, &log_out_query/1)
   end
 
   defp log_out_query(query) do
     "%{cyan}  * #{query["query_id"]}"
-      |> IO.ANSI.escape(true)
-      |> IO.puts
+      |> out
   end
 
   defp write_index(response, configuration, index_name) do
     [name: index_name, id: response["index_id"]]
       |> Configuration.add_index(configuration)
+  end
+
+  defp out(message) do
+    message
+      |> IO.ANSI.escape(true)
+      |> IO.puts
   end
 
   defp index_from_configuration(index_name, configuration) do
